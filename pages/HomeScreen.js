71ed6a7b12acae4fb24ad0bac246a768073c3fe3
Wojciech Swiecicki <wojciech.swiecicki1@gmail.com>
2022-11-01 20:56:37 +0100

    added android build

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -2,18 +2,20 @@ import {Button, Center, Column, Row} from "native-base";
 import themeColors from "../styles/theme";
 import Tile from "../components/Tile";
 import React from "react";
+import {Dimensions} from "react-native";
 
 const HomeScreen = ({navigation}) => {
     return <>
         <Center _dark={{bg: themeColors.darkBlue}} _light={{bg: themeColors.white}} flex={1}>
             <Column space={5} alignItems="center">
                 <Row space={4}>
-                    <Tile text='Espresso' navigation={navigation}/>
-                    <Tile text='V60' navigation={navigation}/>
+                    <Tile text='Espresso' navigation={navigation} size={Dimensions.get('window').width / 3}/>
+                    <Tile text='V60' navigation={navigation} size={Dimensions.get('window').width / 3}/>
                 </Row>
 
-                <Row>
-                    <Tile text={'Bluetooth'} navigation={navigation} path='BLE'/>
+                <Row pt={6}>
+                    <Tile text={'Bluetooth'} navigation={navigation} path='BLE'
+                          size={Dimensions.get('window').width / 4}/>
                 </Row>
             </Column>
         </Center>
